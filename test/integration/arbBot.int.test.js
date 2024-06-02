33e60068579f1a5ba94a38cc940c8bc38addde54
simsim <simeon.campbell@live.com>
2024-06-02 15:31:55 +0700

    integration test setup done

--- test/integration/arbBot.int.test.js
+++ test/integration/arbBot.int.test.js
@@ -11,10 +11,7 @@ const { data: poolsData } = require("../../src/jsonPoolData/uniswapPools.json")
 const artifacts = {
     UniswapV3Router: require("@uniswap/v3-periphery/artifacts/contracts/SwapRouter.sol/SwapRouter.json"),
 }
-const {
-    weth9Abi: weth9Abi,
-    UsdcAbi: UsdcAbi,
-} = require("../mainnetTokens.json")
+const { weth9Abi, UsdcAbi } = require("../mainnetTokens.json")
 
 const ALCHEMY_MAINNET_API = process.env.ALCHEMY_MAINNET_API
 
@@ -26,13 +23,15 @@ const BATCH_INTERVAL = 8000
 WETH_ADDRESS = "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"
 USDC_ADDRESS = "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
 
+const UNISWAP_V3_ROUTER_ADDRESS = "0xE592427A0AEce92De3Edee1F18E0157C05861564"
+
 describe("DualArbBot Tests", function () {
     let deployer
     let weth, usdc, flashswap
 
     beforeEach(async function () {
         // create arb opportunity by swapping weth for usdc
-        ;[deployer] = await ethers.getSigners()
+        ;[deployer] = await hre.ethers.getSigners()
 
         // Impersonate a whale account
         const whale = "0x2feb1512183545f48f6b9c5b4ebfcaf49cfca6f3" // Replace with a WETH or USDC whale address
@@ -51,53 +50,70 @@ describe("DualArbBot Tests", function () {
 
         const whaleUsdcBalance = await usdc.balanceOf(whale)
 
-        console.log(whaleWethBalance.toString())
-        console.log(whaleUsdcBalance.toString())
+        console.log(
+            "Initial whale WETH balance:",
+            hre.ethers.formatEther(whaleWethBalance.toString()),
+        )
+        console.log(
+            "Initial whale USDC balance:",
+            hre.ethers.formatUnits(whaleUsdcBalance.toString(), 6),
+        )
 
-        // create arbitrage opportunity on route[0]
-        // const routerAddress = "0x68b3465833fb72A70ecDF485E0e4C7bD8665Fc45" // Replace with the address of the Uniswap V3 Router contract on the testnet
-        // const router = new hre.ethers.Contract(
-        //     routerAddress,
-        //     artifacts.UniswapV3Router.abi,
-        // )
-        const poolAddress = "0x8ad599c3a0ff1de082011efddc58f1908eb6e6d8" // Replace with the address of the Uniswap V3 Pool contract on the testnet
+        // Transfer WETH from whale to deployer
+        const wethAmount = hre.ethers.parseEther("500")
 
-        // Set the amount of input token to swap
-        const amountIn = hre.ethers.parseEther("500") // Replace with the desired amount of input token
+        // Get the Uniswap V3 Router contract
+        uniswapV3Router = new hre.ethers.Contract(
+            UNISWAP_V3_ROUTER_ADDRESS,
+            artifacts.UniswapV3Router.abi,
+            deployer,
+        )
 
-        // Set the minimum amount of output token to receive
-        const amountOutMin = 0 // Replace with the minimum amount of output token you expect to receive
+        // Approve the router to spend WETH
+        await weth
+            .connect(whaleSigner)
+            .approve(UNISWAP_V3_ROUTER_ADDRESS, wethAmount)
 
-        // Set the deadline for the swap (in Unix timestamp format)
-        const deadline = Math.floor(Date.now() / 1000) + 3600 // Replace with the desired deadline
-
-        // Set the recipient address to receive the swapped tokens
-        const recipient = whaleSigner // Replace with the desired recipient address
-
-        // Approve the router to spend the input token
-        await weth.approve(whaleSigner, amountIn)
-
-        const ExactInputSingleParams = {
+        const params = {
             tokenIn: WETH_ADDRESS,
             tokenOut: USDC_ADDRESS,
             fee: 3000,
-            recipient: whaleSigner,
-            deadline: await ethers.provider
-                .getBlock("latest")
-                .then((block) => block.timestamp + 1000),
-            amountIn: amountIn,
+            recipient: whaleSigner.address,
+            deadline: Math.floor(Date.now() / 1000) + 60 * 10, // 10 minutes from now
+            amountIn: wethAmount,
             amountOutMinimum: 0,
             sqrtPriceLimitX96: 0,
+            // gasLimit: ethers.utils.hexlify(1000000), // Set a high gas limit
+            maxFeePerGas: 9470890005, // Set max fee per gas
+            maxPriorityFeePerGas: 3000000000, // Set priority fee
         }
 
-        const swapTx = await router.exactInputSingle(ExactInputSingleParams)
+        try {
+            console.log(
+                "Executing swap - 500 WETH for USDC to create arb opportunity...",
+            )
+            const tx = await uniswapV3Router
+                .connect(whaleSigner)
+                .exactInputSingle(params)
 
-        // Wait for the swap transaction to be mined
-        await swapTx.wait()
+            await tx.wait()
+            console.log("Swap executed successfully")
+        } catch (error) {
+            console.error("Error executing exactInputSingle:", error)
+        }
 
-        // Check the balance of the output token after the swap
-        const outputTokenBalance = await usdc.balanceOf(recipient)
-        console.log("Output Token Balance:", outputTokenBalance.toString())
+        // Verify balances
+        const newWhaleWethBalance = await weth.balanceOf(whaleSigner.address)
+        const newWhaleUsdcBalance = await usdc.balanceOf(whaleSigner.address)
+        console.log("--------------------")
+        console.log(
+            "New Whale WETH balance:",
+            hre.ethers.formatEther(newWhaleWethBalance.toString()),
+        )
+        console.log(
+            "New Whale USDC balance:",
+            hre.ethers.formatUnits(newWhaleUsdcBalance.toString(), 6),
+        )
     })
     it("runs tests", async function () {
         console.log("testing...")
