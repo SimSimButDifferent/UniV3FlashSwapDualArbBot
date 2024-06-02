aa35b794734ff36c676ed0e7f8b7914d131ae824
simsim <simeon.campbell@live.com>
2024-06-02 14:41:30 +0700

    reverted

--- test/arbBot.test.js
+++ test/arbBot.test.js
@@ -1,12 +1,28 @@
-const hre = require("hardhat")
+const { ethers, network } = require("hardhat")
 const { expect } = require("chai")
 
+<<<<<<< HEAD:test/unit/arbBot.u.test.js
 const { arbQuote } = require("../../src/utils/arbQuote")
 const { poolInformation } = require("../../src/utils/poolInformation")
 const { initPools } = require("../../src/utils/InitPools")
 const { findArbitrageRoutes } = require("../../src/utils/findArbitrageRoutes")
 
 const { data: poolsData } = require("../../src/jsonPoolData/uniswapPools.json")
+=======
+const { dualArbScan } = require("../src/dualArbScan")
+const { arbQuote } = require("../src/utils/arbQuote")
+const { poolInformation } = require("../src/utils/poolInformation")
+const { initPools } = require("../src/utils/InitPools")
+const { findArbitrageRoutes } = require("../src/utils/findArbitrageRoutes")
+
+const { data: poolsData } = require("../src/jsonPoolData/uniswapPools.json")
+const {
+    weth9Abi: weth9Abi,
+    UsdcAbi: UsdcAbi,
+} = require("../test/mainnetTokens.json")
+
+const ALCHEMY_MAINNET_API = process.env.ALCHEMY_MAINNET_API
+>>>>>>> parent of 68302ae... integration test folder added + hre added to tests:test/arbBot.test.js
 
 const pools = poolsData.pools
 const amountInUsd = "100"
@@ -16,6 +32,26 @@ describe("DualArbBot Tests", function () {
 
     beforeEach(async function () {
         ;[deployer] = await ethers.getSigners()
+
+        // Impersonate a whale account
+        const whale = "0x2feb1512183545f48f6b9c5b4ebfcaf49cfca6f3" // Replace with a WETH or USDC whale address
+        await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [whale],
+        })
+
+        const whaleSigner = await ethers.getSigner(whale)
+
+        // // Get the WETH and USDC contracts
+        weth = new ethers.Contract(WETH_ADDRESS, weth9Abi, deployer)
+        usdc = new ethers.Contract(USDC_ADDRESS, UsdcAbi, deployer)
+
+        const whaleWethBalance = await weth.balanceOf(whale)
+
+        const whaleUsdcBalance = await usdc.balanceOf(whale)
+
+        console.log(whaleWethBalance.toString())
+        console.log(whaleUsdcBalance.toString())
     })
     describe("getPools", function () {
         it("Should correctly create uniswapPools json file", async function () {
@@ -35,7 +71,7 @@ describe("DualArbBot Tests", function () {
     describe("findArbitrageRoutes", function () {
         let tokenAmountsIn
         it("Should correctly find arbitrage routes", async function () {
-            await initPools(pools)
+            const poolsArray = await initPools(pools)
 
             tokenAmountsIn = await poolInformation(pools, amountInUsd)
 
@@ -52,24 +88,28 @@ describe("DualArbBot Tests", function () {
     describe("arbQuote", function () {
         let route, amountIn, routeNumber
         before(async function () {
-            await initPools(pools)
+            const poolsArray = await initPools(pools)
             const tokenAmountsIn = await poolInformation(pools, amountInUsd)
             const routesArray = await findArbitrageRoutes(
                 pools,
                 tokenAmountsIn,
                 amountInUsd,
             )
-            route = routesArray[9]
+            route = routesArray[0]
             amountIn = route[7]
             routeNumber = 0
             profitThreshold = route[8]
+<<<<<<< HEAD:test/unit/arbBot.u.test.js
             
+=======
+>>>>>>> parent of 68302ae... integration test folder added + hre added to tests:test/arbBot.test.js
         })
         it("Should correctly calculate the arbitrage quote", async function () {
-            const quote = await arbQuote(route, amountIn, routeNumber)
+            // create arbitrage opportunity on route[0]
 
-            expect(quote).to.be.an("array")
-            expect(quote[0]).to.be.a("BigInt")
+            // const quote = await arbQuote(route, amountIn, routeNumber)
+
+            console.log("route", route)
         })
     })
 })
