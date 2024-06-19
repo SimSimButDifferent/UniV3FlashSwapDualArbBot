53739f12c5b053db174442cc01e00127693c1029
simsim <simeon.campbell@live.com>
2024-06-19 15:11:26 +0700

    arb subgraph query done

--- src/utils/getPoolsArbitrum.js
+++ src/utils/getPoolsArbitrum.js
@@ -1,10 +1,6 @@
 const fs = require("fs")
 
-// The addresses of the tokens used to create the arb routes.
-
-const USDC = `"0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"`
-const USDT = `"0xdac17f958d2ee523a2206206994597c13d831ec7"`
-const WETH = `"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"`
+const SUBGRAPH_API_KEY = process.env.SUBGRAPH_API_KEY
 
 /**
  * @dev This function gets the pools from the Uniswap V3 subgraph
@@ -12,19 +8,51 @@ const WETH = `"0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"`
  */
 async function getPools() {
     // The query to get the pools from the Uniswap V3 subgraph
+    //   const query = `
+    //   {
+    //     pools(
+    //       where: {
+    //         token0_in: [
+    //           ${USDT}, ${USDC}, ${WETH}
+    //         ]
+    //         token1_in: [
+    //           ${USDT}, ${USDC}, ${WETH}
+    //         ]
+    //         totalValueLockedUSD_gt: 1000000
+    //       }
+    //       first: 1000
+    //     ) {
+    //       id
+    //       feeTier
+    //       totalValueLockedUSD
+    //       tick
+    //       token0 {
+    //         id
+    //         symbol
+    //         name
+    //         decimals
+    //       }
+    //       token1 {
+    //         id
+    //         symbol
+    //         name
+    //         decimals
+    //       }
+    //       token0Price
+    //       token1Price
+    //     }
+    //   }
+    // `
     const query = `
     {
       pools(
         where: {
-          token0_in: [
-            ${USDT}, ${USDC}, ${WETH}
-          ]
-          token1_in: [
-            ${USDT}, ${USDC}, ${WETH}
-          ]
-          totalValueLockedUSD_gt: 1000000
+          volumeUSD_gte: 500000,
+          id_not: "0x14af1804dbbf7d621ecc2901eef292a24a0260ea"
         }
-        first: 1000
+        first:7,
+        orderBy: totalValueLockedUSD,
+        orderDirection: desc
       ) {
         id
         feeTier
@@ -48,7 +76,7 @@ async function getPools() {
     }
   `
     // Subgraph URL
-    const url = "https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v3"
+    const url = `https://gateway-arbitrum.network.thegraph.com/api/${SUBGRAPH_API_KEY}/subgraphs/id/FbCGRftH4a3yZugY7TnbYgPJVEv2LvMT6oF1fxPe9aJM`
 
     // Log the response from the subgraph
     const response = await fetch(url, {
@@ -64,7 +92,7 @@ async function getPools() {
     // Write the response to a file
 
     fs.writeFileSync(
-        "src/jsonPoolData/uniswapPools.json",
+        "src/jsonPoolData/ArbitrumUniPools.json",
         JSON.stringify(jsonDict, null, 2),
     )
 
