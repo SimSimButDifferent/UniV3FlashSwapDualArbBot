ea971bece5c22fde3fa6e67ac254636ac2c6bee9
simsim <simeon.campbell@live.com>
2024-06-20 07:57:30 +0700

    provider updated to arbitrum

--- src/utils/getProvider.js
+++ src/utils/getProvider.js
@@ -1,13 +1,13 @@
 const ethers = require("ethers")
 require("dotenv").config()
 
-// The Alchemy API URL from .env file
-const ALCHEMY_MAINNET_API = process.env.ALCHEMY_MAINNET_API
+// The ARBITRUM API URL from .env file
+const ARBITRUM_MAINNET_API = process.env.ARBITRUM_MAINNET_API
 
 // Function to get the provider
 function getProvider() {
-    // const provider = new ethers.providers.JsonRpcProvider(ALCHEMY_MAINNET_API)
-    const provider = new ethers.JsonRpcProvider(ALCHEMY_MAINNET_API)
+    // const provider = new ethers.providers.JsonRpcProvider(ARBITRUM_MAINNET_API)
+    const provider = new ethers.JsonRpcProvider(ARBITRUM_MAINNET_API)
 
     return provider
 }
